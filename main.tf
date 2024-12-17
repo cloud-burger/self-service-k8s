93802ffabb5eef76c55f9c1334e59bc2ce78760a
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-12-16 23:41:31 -0300

    refactor: remove state reference

--- main.tf
+++ main.tf
@@ -11,24 +11,6 @@ module "global_variables" {
   environment = var.environment
 }
 
-data "terraform_remote_state" "database_state" {
-  backend = "s3"
-
-  config = {
-    bucket = "cloud-burger-state"
-    key    = "prod/database.tfstate"
-    region = "us-east-1"
-  }
-}
-
-locals {
-  aws_vpc_id            = data.terraform_remote_state.database_state.outputs.vpc_id
-  aws_public_subnet_id  = data.terraform_remote_state.database_state.outputs.public_subnet_id
-  aws_public_subnet2_id = data.terraform_remote_state.database_state.outputs.public_subnet2_id
-  aws_private_subnet_id = data.terraform_remote_state.database_state.outputs.private_subnet_id
-  aws_rds_public_sg_id  = data.terraform_remote_state.database_state.outputs.rds_public_sg_id
-}
-
 provider "aws" {
   region = module.global_variables.aws_region
 }
