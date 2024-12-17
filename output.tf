c6629de71d3bc6262e5100b8e2cfdadfdbf1aa07
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-12-17 02:28:15 -0300

    chore: export outputs

--- output.tf
+++ output.tf
@@ -9,3 +9,15 @@ output "public_subnets" {
 output "private_subnets" {
   value = module.vpc.private_subnets
 }
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_certificate" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
